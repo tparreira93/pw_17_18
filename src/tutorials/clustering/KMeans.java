2e10585dedd2e7b482469a4b43d6cdcf2744da7a
tsanto <tf.santo@campus.fct.unl.pt>
2017-12-15 02:24:16 +0000

    kmeans

--- src/tutorials/clustering/KMeans.java
+++ src/tutorials/clustering/KMeans.java
@@ -1,47 +1,75 @@
 package tutorials.clustering;
 
-import org.carrot2.clustering.kmeans.BisectingKMeansClusteringAlgorithm;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.carrot2.core.Cluster;
 import org.carrot2.core.Document;
 
-import java.util.LinkedList;
-import java.util.List;
+
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.carrot2.clustering.kmeans.BisectingKMeansClusteringAlgorithm;
 
 public class KMeans {
-	
 
-	public static void clusterData() {
-		
-		List<org.apache.lucene.document.Document> docs;
-		
+	public static List<ScoreDoc> clusterData(List<ScoreDoc> docs, IndexSearcher searcher) throws IOException {
+
 		BisectingKMeansClusteringAlgorithm b = new BisectingKMeansClusteringAlgorithm();
 		List<Document> documents = new LinkedList<Document>();
-		documents.add(new Document("WordA . WordA"));
-		documents.add(new Document("WordB . WordB"));
-		documents.add(new Document("WordC . WordC"));
-		documents.add(new Document("WordA . WordA"));
-		documents.add(new Document("WordB . WordB"));
-		documents.add(new Document("WordC . WordC"));
+
+		Map<Integer, Float> scores = new HashMap<>();
+		for (ScoreDoc d : docs) {
+
+			org.apache.lucene.document.Document s = searcher.doc(d.doc);
+			Document a = new Document(s.getField("Body").toString().split("<Body:")[1]);
+			a.setField("IdLucene", d.doc);
+			double score = d.score;
+			//System.out.println(" SCORE:" + d.score +" Body: "+s.getField("Body").toString().split("<Body:")[1]);
+			scores.put(d.doc, d.score);
+			a.setScore(score);
+			documents.add(a);
+		}
 
 		b.documents = documents;
-		b.labelCount=1;
-		b.partitionCount= 3;
+		b.labelCount = 1;
+		b.partitionCount = 4;
+		b.clusterCount = 3;
+
 		b.process();
 		List<Cluster> cluster = b.clusters;
-		
-		for(Cluster c: cluster){
-			System.out.println(c.getLabel());
+
+		List<Document> best = new LinkedList<Document>();
+
+		for (Cluster c : cluster) {
+			List<Document> temp = c.getDocuments();
+			double score = Double.MIN_VALUE;
+			Document bestDoc = null;
+			for (int i = 0; i < temp.size(); i++) {
+				Document b1 = temp.get(i);
+				if (score < b1.getScore()) {
+					score = b1.getScore();
+					bestDoc = b1;
+				}
+
+			}
+
+			best.add(bestDoc);
 		}
-	
-		
+
+		List<ScoreDoc> results = new LinkedList<ScoreDoc>();
+		System.out.println("\n");
+		for (Document d : best) {
+			System.out.println("Score:" + d.getScore() + " title:" + d.getTitle());
+			int id = d.getField("IdLucene");
+			results.add(new ScoreDoc(id, scores.get(id)));
+		}
+
+		return results;
+
 	}
-	
-	 public static void main(String[] args) {
-		 
-		 /*Logger logger = Logger.getLogger(KMeans.class);
-		 BasicConfigurator.configure();*/
-		 //logger.info("This is my first log4j's statement");
-		 clusterData();
-	 }
 
 }
